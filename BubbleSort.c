0565e9b724fea53e697c6523d827158fc6078ae9
sachinijayasundara <91739119+sachinijayasundara@users.noreply.github.com>
2021-10-03 19:03:24 +0800

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,29 +1,33 @@
-#include <studio.h>
+#include<stdio.h>
 
+int main() {
 
-int main()
-{
-    bool swap;
-    int n=5,j;
-    int arr[5]={58,89,71,35,6};
-    
-    
-    do{
-        swap=false;
-        for(j=0;j<n;j++)
-        {
-            if (arr[j]>arr[j-1]);
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
-            }
+   int count, temp, i, j, number[30];
+
+   printf("How many numbers are u going to enter?: ");
+   scanf("%d",&count);
+
+   printf("Enter %d numbers: \n",count);
+
+   for(i=0;i<count;i++) {
+        scanf("%d",&number[i]);
+   }
+
+   for(i=count-2;i>=0;i--) {
+      for(j=0;j<=i;j++) {
+        if(number[j]>number[j+1]) {
+           temp=number[j];
+           number[j]=number[j+1];
+           number[j+1]=temp;
         }
-    }while(swap);
+      }
+   }
+
+   printf("Sorted elements: \n");
     
-    for(m=0;m<n;m--)
-        {
-            printf("%s ",arr[l]);
-        }
+   for(i=0;i<count;i++) {
+      printf("%d ",number[i]);
+   }
+    
+   return 0;
 }
